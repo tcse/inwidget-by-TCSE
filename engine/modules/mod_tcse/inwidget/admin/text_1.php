2c1911a3299d2baa4060523ee0819c990664a136
TCSE <talik@tcse-cms.com>
2020-05-10 00:39:52 +0300

    Версия 0.2.0 - добавлена видеоинструкция по получению ACCESS TOKEN и inwidget Последняя версия 1.3.3. от 15 марта 2020 г.

--- engine/modules/mod_tcse/inwidget/admin/text_1.php
+++ engine/modules/mod_tcse/inwidget/admin/text_1.php
@@ -51,23 +51,30 @@ if (!defined('DATALIFEENGINE') || !defined('LOGGED_IN')) {
 		</div>
 	</div>
 	<div class="clear">&nbsp;</div>
-	<div class="download-box">
-		<a href="http://inwidget.ru/download.php?version=inwidget-current" target="_blank" title="Скачать виджет" class="download" onclick="yaCounter23803873.reachGoal(&quot;gotodownload&quot;);">
-			СКАЧАТЬ ВИДЖЕТ &gt;
-			<div class="version">INWIDGET-1.3.1.zip</div>
-		</a>
-	</div>
 	<div class="notice">
 		<div style="color:red;">
-			Последняя версия 1.3.1. от 05 февраля 2019 г.
+			Последняя версия 1.3.3. от 15 марта 2020 г.
 		</div>
 		<div class="github">
 			<!--<img src='/i/github.gif' alt='' />-->
-			<a href="https://github.com/aik27/inwidget" target="_blank" onclick="yaCounter23803873.reachGoal(&quot;gotogit&quot;);">Исходный код на GitHub &gt;</a>
+			<a href="https://github.com/aik27/inwidget" target="_blank">Исходный код на GitHub &gt;</a>
 			<div class="clear">&nbsp;</div>
 		</div>
 	</div>
 	<div class="clear">&nbsp;</div>
+
+	<h3>Видеоинструкция по получению ACCESS TOKEN:</h3>
+	<p>
+		Виджет может работать с двумя видами API (недокументированное и endpoints). По умолчанию используется недокументированное. Для его работы токен не требуется. Указание ACCESS TOKEN в настройках виджета переводит его на режим работы с Endpoints API (https://www.instagram.com/developer/). Если вы хотите создать собственное приложение в Instagram, то воспользуйтесь видеоинструкцией ниже. Имейте ввиду, что ваше приложение сначала попадёт в «песочницу» со следующими лимитами: 20 – максимальное количество фотографий, которое можно получить в запросе. 500 – максимальное количество запросов в час. И самое главное - фотографии можно получить только из своего аккаунта. Тоже самое касается выборки по тегам.
+	</p>
+	<iframe width="400" height="225" src="https://www.youtube.com/embed/_O669Dx3djw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+	<div style="padding:20px;background:#f6f6f6;width:760px;border-radius:5px 5px 5px 5px;margin:20px 0 50px 0;overflow-x:scroll;">
+		<p><b>Зарегистрировать собственное приложение можно здесь:</b></p>
+		<p style="margin-bottom:5px;"><a href="https://www.instagram.com/developer/" target="_blank">https://www.instagram.com/developer/</a></p>
+		<p><b>URL для генерации ACCESS TOKEN:</b></p>
+		https://www.instagram.com/oauth/authorize/?client_id=<b class="red">ВАШ_CLIENT_ID</b>&amp;redirect_uri=<b class="red">ВАШ_URL_АВТОРИЗАЦИИ</b>&amp;response_type=token&amp;scope=basic
+	</div>
+
 </div>
 
 
